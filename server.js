4b9152712313fa90fe8face6e7e190a093be9840
wahbafarag <wahba.farag01@gmail.com>
2023-07-19 18:00:16 +0200

    configenv

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ dotenv.config({ path: path.join(__dirname, 'config.env') });
 const app = require('./app');
 
 mongoose
-  .connect('mongodb://localhost:27017/new-natgiours', {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
