2c62023ae72febcadc5bf01c2bd00249d80eb7d6
wahbafarag <wahba.farag01@gmail.com>
2023-07-21 21:30:59 +0200

    improving webhooks

--- controllers/bookingControllers.js
+++ controllers/bookingControllers.js
@@ -70,7 +70,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async () => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].unit_amount;
+  const price = session.line_items[0].unit_amount / 100;
   await Booking.create({ tour, user, price });
 };
 
@@ -87,7 +87,7 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook Error: ${err.message}`);
   }
 
-  if (event.type === 'checkout.session.complete')
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
   res.status(200).json({
